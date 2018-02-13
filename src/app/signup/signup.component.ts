d7110235a4cb19e3d24b010c3d4e007107a84972
winter-sleep <icyo_dec@163.com>
2018-02-14 01:28:17 +0800

    signup page

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SignupData } from './struct';
+import { SignupData, FieldState } from './struct';
 
 @Component({
   selector: 'app-signup',
@@ -15,24 +15,94 @@ export class SignupComponent implements OnInit {
       identify: '',
       nickname: '',
       password: '',
+      repassword: '',
       gender: 0
   };
+
+  public fieldState: FieldState = {
+      email: true,
+      identify: true,
+      nickname: true,
+      password: true,
+      repassword: true,
+      gender: true
+  };
+
+  private emailTouched = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  public stepOneHandle() {
+  public stepOneHandle(): void {
     this.currentStep = 1;
   }
 
-  public stepTwoHandle() {
-    this.currentStep = 2;
+  public stepTwoHandle(): void {
+    if (
+      this.isEmailValide() &&
+      this.isIdentifyValide()
+    ) {
+      this.matchIdentify((data:any) => {
+        if (this.fieldState.identify) {
+          this.currentStep = 2;
+        }
+      })
+    }
   }
 
-  public stepThreeHandle() {
+  public stepThreeHandle(): void {
     this.currentStep = 3;
+  }
+
+  public postHandle(): void {
+    console.log(this.formData);
+  }
+
+/**
+ * 验证 email 格式
+ * 格式正确改变状态
+ */
+  private isEmailValide(): Boolean {
+    let reg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,4}){1,2})$/;
+    if (!reg.test(this.formData.email)) {
+        this.fieldState.email = false;
+    } else {
+        this.fieldState.email = true;
+    }
+    return this.fieldState.email;
+  }
+/**
+* 在 email 表单失去焦点时进行验证
+* 行为与 SignupComponent::isEmailValide 一致
+*/
+  public emailTouchedHandle(): void {
+    this.isEmailValide();
+  }
+
+/**
+* 提供验证码格式验证功能
+*/
+  private isIdentifyValide(): Boolean {
+    let reg = /^[a-zA-Z0-9]{6}$/;
+    if (reg.test(this.formData.identify)) {
+      this.fieldState.identify = true;
+    } else {
+      this.fieldState.identify = false;
+    }
+    return this.fieldState.identify;
+  }
+
+/**
+ * 调用接口检测验证码是否匹配, 通过 callback 传递验证信息
+ * @param callback string 异步验证的回调函数
+ */
+  private matchIdentify(callback: Function): void {
+    setTimeout(() => {
+      this.fieldState.identify = true;
+      callback();
+    }, 1000);
   }
 
 }
--- src/app/signup/struct.ts
+++ src/app/signup/struct.ts
@@ -2,9 +2,19 @@
  * 注册表单数据模型
  */
 export class SignupData {
-    email: string;
-    nickname: string;
-    password: string;
-    gender: number;
-    identify: string;
+    public email: string;
+    public nickname: string;
+    public password: string;
+    public repassword: string;
+    public gender: number;
+    public identify: string;
+}
+
+export class FieldState {
+    public email: Boolean;
+    public nickname: Boolean;
+    public password: Boolean;
+    public repassword: Boolean;
+    public gender: Boolean;
+    public identify: Boolean;
 }
