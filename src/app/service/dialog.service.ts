972231460952178209cc5151629c5861f7284188
winter-sleep <icyo_dec@163.com>
2018-02-24 19:34:36 +0800

    sadas

--- src/app/service/dialog.service.ts
+++ src/app/service/dialog.service.ts
@@ -30,10 +30,12 @@ export class DialogService {
 
   private init(): void {
     this.title = 'Fragment 提醒您';
-    this.content = '';
+    this.content = '您的邮件尚未激活，请需要前往邮箱查收激活邮件，点击确认重新发送邮件～';
     this.funcText = '确 定';
     this.confirmLink = 'javascript:void(0)';
-    this.confirmFunction = () => void(0);
+    this.confirmFunction = () => {
+      alert(111);
+    };
   }
 
 }
