36bd1237fe8f8d423bf337578e804076774cbed8
winter-sleep <icyo_dec@163.com>
2018-02-26 01:06:53 +0800

    asdasd

--- src/app/lib/active/active.component.ts
+++ src/app/lib/active/active.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-active',
   templateUrl: './active.component.html',
-  styleUrls: ['./active.component.css']
+  styleUrls: ['./active.component.scss']
 })
 export class ActiveComponent implements OnInit {
+
+  public email: String;
+  public identify: String;
+  public captchaImage: String;
 
   constructor() { }
 
   ngOnInit() {
+    this.changeCaptcha();
+  }
+
+  public submitHandle() {
+    alert(1);
+  }
+
+  public changeCaptcha(): void {
+    const date = new Date();
+    this.captchaImage = '/api/user/captcha?date=' + Date.parse(date.toString());
+    this.identify = '';
   }
 
 }
